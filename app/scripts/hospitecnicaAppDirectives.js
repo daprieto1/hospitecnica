004135e869c3ab2134f65c9112bd15f641ad8fba
daprieto1 <da.prieto1@uniandes.edu.co>
2017-12-29 23:30:37 -0500

    create maintenance window

--- app/scripts/hospitecnicaAppDirectives.js
+++ app/scripts/hospitecnicaAppDirectives.js
@@ -8,10 +8,41 @@
                 console.log(backStyle);
                 element.css({
                     'background': backStyle,
-                    'background-size': 'cover',                       
+                    'background-size': 'cover',
                     'background-position': 'center',
                     'min-height': minHeight
                 });
             };
+        })
+
+        .directive('contactForm', function () {
+            var controller = ['$scope', '$firebaseArray', function ($scope, $firebaseArray) {
+
+                $scope.sendContact = () => {
+                    console.log($scope.contact);
+                    $scope.leadsFirebaseArray.$add($scope.contact)
+                        .then(ref => {
+                            $scope.contactForm.$setPristine();
+                            $scope.contact = {};
+                        })
+                        .catch(err => console.log(err));
+                };
+
+                function initCtrl() {
+                    $scope.leadsFirebaseArray = $firebaseArray(firebase.database().ref().child('leads'));
+                    $scope.leadsFirebaseArray.$loaded()
+                        .then(leads => console.log(leads))
+                        .catch(err => console.log(err));
+
+                    $scope.contact = {};
+                }
+
+                initCtrl();
+            }];
+
+            return {
+                controller: controller,
+                templateUrl: 'views/directives/contactForm.html'
+            }
         });
 })();
